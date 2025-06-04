a05ea2f1f68091853498de6106ef3f2d858155c8
Fidih <fidihartato.pidi@gmail.com>
2025-06-04 21:36:26 +0700

    day 3 latihan width dan height serta sedikit flex

---
+++ belajar/day3/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+const path = require("path");
+
+module.exports = {
+  content: [
+    path.join(__dirname, "src/**/*.{html,js}"),
+    path.join(__dirname, "dist/**/*.html")
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
